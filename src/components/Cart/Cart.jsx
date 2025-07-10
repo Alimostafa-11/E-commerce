36ff36e1c6eba79da94051309ccaaddbc5ba4663
Ali <alimostafa1712004yahoo@gmail.com>
2025-07-10 21:45:07 +0300

    Update home page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -59,25 +59,6 @@ const Cart = () => {
 
           <div className="relative pt-10 overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              {/* <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-16 py-3">
-                    <span className="sr-only">Image</span>
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Product
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Qty
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Price
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Action
-                  </th>
-                </tr>
-              </thead> */}
               <tbody>
                 {products?.map((prod) => {
                   return (
--- src/components/WishList/WishList.jsx
+++ src/components/WishList/WishList.jsx
@@ -46,25 +46,6 @@ const WishList = () => {
         <div className="relative pt-10 overflow-x-auto shadow-md sm:rounded-lg">
           <h2 className="text-4xl pb-3 text-green-500">My Wish List</h2>
           <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            {/* <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="px-16 py-3">
-                  <span className="sr-only">Image</span>
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Product and title
-                </th>
-                <th scope="col" className="px-6 text-center py-3">
-                  delete
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Price
-                </th>
-                <th scope="col" className="px-6  py-3">
-                  Action
-                </th>
-              </tr>
-            </thead> */}
             <tbody>
               {displayedProducts?.map((prod) => (
                 <tr key={prod?._id} className="bg-white border-b  ">
